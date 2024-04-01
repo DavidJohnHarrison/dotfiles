59100d140b27fb2b4e951d2ca9ec518ab24f1aa5
David John Harrison <david@cintian.ai>
2024-04-01 06:47:18 +0100

    NVim: Show hidden files in Oil

--- .config/nvim/after/plugin/oil.lua
+++ .config/nvim/after/plugin/oil.lua
@@ -1,6 +1,9 @@
 local oil = require("oil")
 oil.setup({
     default_file_explorer = false,
+    view_options = {
+        show_hidden = true,
+    },
 })
 
 --vim.keymap.set('n', '<leader>R', oil.toggle_float)
