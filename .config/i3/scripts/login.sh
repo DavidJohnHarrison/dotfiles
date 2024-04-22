8e668661d9ed41f23b8aac6c78c4a58822c3ab0d
David John Harrison <david@cintian.ai>
2024-04-22 12:59:14 +0100

    i3: BUGFIX - Working set not reset on login
    - Login script was adding the "cleared" working set file to wrong
      directory (current directory, which on login is `~` and not the
      direcotry of the script). Fix this by adding the full path to the
      correct file.
    - Delete the `.current_working_set` file, as this should not be version
      controlled.

--- .config/i3/scripts/login.sh
+++ .config/i3/scripts/login.sh
@@ -7,6 +7,6 @@ setxkbmap -option "caps:escape"
 # Initial workspace configuration
 echo "F1" > ~/.config/i3/scripts/.current_workspace_group
 echo '`' > ~/.config/i3/scripts/.current_workspace
-echo '' > .current_working_set
+echo '' > ~/.config/i3/scripts/.current_working_set
 i3-msg workspace F1_\`
 #./switch_workspace.sh 1
