c0fbedbc0ab7f5867199ce22cd717539cd5243c5
David John Harrison <david@harrison.sh>
2024-08-29 18:25:14 +0100

    NVim: `null-ls` mypy fix for virtual environments

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -3,7 +3,12 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.ruff,
-        null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.diagnostics.mypy.with({
+            extra_args = function()
+                local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
+                return { "--python-executable", virtual .. "/bin/python3" }
+            end,
+        }),
         null_ls.builtins.diagnostics.ruff,
     },
     on_attach = function(client, bufnr)
